67fce94f36e741f28db6994652ae6ad787a60e69
Nicolás <nicolasleonardelli2012@gmail.comm>
2022-07-01 07:49:58 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,16 @@
+const { json } = require('express');
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",(req,res,next)=>{
+    res-json({response:true})
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Escuchando en http://localhost:${PORT}`)
+})
